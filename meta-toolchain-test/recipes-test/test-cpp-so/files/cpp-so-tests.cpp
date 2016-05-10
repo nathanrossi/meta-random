b94e0140b6f767152a3d31ebec4ad3946db8a053
Nathan Rossi <nathan@nathanrossi.com>
2016-05-10 23:54:34 +1000

    meta-toolchain-test/test-cpp-so: Test CPP shared linking

---
+++ meta-toolchain-test/recipes-test/test-cpp-so/files/cpp-so-tests.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "test iostream so linking" << std::endl;
+
+	return 0;
+}
