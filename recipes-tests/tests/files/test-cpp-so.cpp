d9c0d37d4dce4b1b591591f2f9894ac73ebeadf0
Nathan Rossi <nathan@nathanrossi.com>
2017-06-10 22:39:52 +1000

    recipes-tests/tests/files/test-cpp-so: Add some more complexity
    Signed-off-by: Nathan Rossi <nathan@nathanrossi.com>

--- recipes-tests/tests/files/test-cpp-so.cpp
+++ recipes-tests/tests/files/test-cpp-so.cpp
@@ -3,9 +3,21 @@
 #include <stdint.h>
 
 #include <iostream>
+#include <vector>
 
 int main(int argc, char* argv[])
 {
+	std::vector<int> test_vec;
+
+	test_vec.push_back(0);
+	test_vec.push_back(32767);
+	test_vec.push_back(-32768);
+
+	for (std::vector<int>::iterator it = test_vec.begin(); it != test_vec.end(); ++it)
+	{
+		std::cout << "testing iostream and c++ vector, value " << *it << std::endl;
+	}
+
 	std::cout << "test iostream so linking" << std::endl;
 
 	return 0;
